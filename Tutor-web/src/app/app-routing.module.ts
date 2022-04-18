5008827c395ffa95a0fbf7f2e6bd68f25bc9cd6f
Roberto Pedroza <Roberto.Pedroza@autozone.com>
2022-04-18 14:33:01 -0600

    Search bar v1

--- Tutor-web/src/app/app-routing.module.ts
+++ Tutor-web/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './components/signup/signup.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
-  { path: 'signup', component: SignupComponent }
+  { path: 'signup', component: SignupComponent },
+  { path: 'search', component: SearchComponent}
 ];
 
 @NgModule({
--- Tutor-web/src/app/app.module.ts
+++ Tutor-web/src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { SearchComponent } from './components/search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
